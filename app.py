949ec899a3118b6d3e719c7511427cc8929fe2e0
Daniel Lopez <a315120@uach.mx>
2021-08-30 09:56:58 -0500

    get y post

--- app.py
+++ app.py
@@ -1,16 +1,21 @@
 import tornado.ioloop
 import tornado.web
 
-class MainHandler(tornado.web.RequestHandler):
+class HelloHandler(tornado.web.RequestHandler):
     def get(self):
-        self.write("Hello, world")
+        self.write("Hola, profe)
+
+    def post(self):
+        user = self.get_argument("username")
+        passwd = self.get_argument("password")
+        self.write("Your username is %s and password is %s" % (user, passwd))
 
 def make_app():
     return tornado.web.Application([
-        (r"/", MainHandler),
+        (r"/", HolaMundo),
     ])
 
 if __name__ == "__main__":
     app = make_app()
-    app.listen(8888)
+    app.listen(8080)
     tornado.ioloop.IOLoop.current().start()
